e515087f2cdbf467795d91016ab56a2b96ed37b4
Esteban <esteban.racana@gmail.com>
2025-09-05 15:45:31 +0200

    Se crearon los botones necesarios para el mini juego

--- js/piedra-papel-tijera.js
+++ js/piedra-papel-tijera.js
@@ -1,29 +1,28 @@
-const minijuego_2=document.querySelector(".minijuego_2");
-let jugador;
-let computadora;
-const button=document.getElementById("button");
-button.textContent="Jugar";
+//Pone las constantes y un array de opciones
+const minijuego_2 = document.querySelector(".minijuego_2"); 
 const opciones = ['Piedra', 'Papel', 'Tijera'];
-button.addEventListener("click",()=>{  
-    jugador = prompt("Elige: Piedra, Papel o Tijera");
-    computadora = opciones[Math.floor(Math.random() * opciones.length)];
-    if(jugador === computadora){     
+
+//Hace un forEach para crear botones
+opciones.forEach(opcion => {
+    const btn = document.createElement("button"); //Aca crea el primer boton
+    btn.textContent = opcion; //Muestra los botones con sus respectivos nombres como Piedra, papel o Tijera
+    btn.addEventListener("click", () => jugar(opcion)); //Clickea y ejecuta jugar
+    minijuego_2.appendChild(btn);  //Agrega los botones al div minijuego_2
+});
+//Hace la funcion jugar que adentro esta la variable jugador y computadora
+function jugar(jugador) {
+    //Genera una eleccion aleatorio de la computadora a traves de las opciones del array
+    const computadora = opciones[Math.floor(Math.random() * opciones.length)];
+    //Hace comparaciones entre el jugador y la computadora y muestra los mensajes necesarios
+    if (jugador === computadora) {     
         alert("Empate! Ambos eligieron " + jugador);
-}else if(jugador === 'Piedra' && computadora=='Papel'){
-    alert("Perdiste! La computadora eligió " + computadora);
-}else if(jugador === 'Piedra' && computadora=='Tijera'){
-    alert("Ganaste! La computadora eligió " + computadora);
-}else if(jugador === 'Papel' && computadora=='Tijera'){
-    alert("Perdiste! La computadora eligió " + computadora);
-}else if(jugador === 'Papel' && computadora=='Piedra'){
-    alert("Ganaste! La computadora eligió " + computadora);
-}else if(jugador === 'Tijera' && computadora=='Piedra'){
-    alert("Perdiste! La computadora eligió " + computadora);
-}else if(jugador === 'Tijera' && computadora=='Papel'){
-    alert("Ganaste! La computadora eligió " + computadora);
-}else if(jugador != 'Piedra' && jugador != 'Papel' && jugador != 'Tijera'){
-    alert("Elección inválida. Por favor, elige Piedra, Papel o Tijera.");
-    IniciarJuego();;
-}else{
-alert("Error, ingreso de datos incorrecto.")
-}});
+    } else if (
+        (jugador === 'Piedra' && computadora === 'Tijera') ||
+        (jugador === 'Papel' && computadora === 'Piedra') ||
+        (jugador === 'Tijera' && computadora === 'Papel')
+    ) {
+        alert("Ganaste! La computadora eligió " + computadora);
+    } else {
+        alert("Perdiste! La computadora eligió " + computadora);
+    }
+}
